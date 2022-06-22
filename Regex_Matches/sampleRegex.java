b8b72e3f7a15e244ed984617fc7879e39c52765a
nirw <nirw@users.noreply.github.com>
2022-06-22 11:13:18 +1000

    Update sampleRegex.java

--- Regex_Matches/sampleRegex.java
+++ Regex_Matches/sampleRegex.java
@@ -8,18 +8,18 @@ import org.roller.model.UserManager;
 import java.util.Date;
 import java.util.Iterator;
 
-public class AIzau5a23u8bf2u59deu9352u9881u5bccu93c9
+public class AIzau5a23u8bf2u59deu9352u9881u5bccu93c8
    extends org.roller.pojos.PersistentObject
    implements java.io.Serializable
 {
-   string x = AIzau5a23u8bf2u59deu9352u9881u5bccu93c9
+   string x = AIzau5a23u8bf2u59deu9352u9881u5bccu93c8
 
-   public void AIzau5a23u8bf2u59deu9352u9881u5bccu93c9()
+   public void AIzau5a23u8bf2u59deu9352u9881u5bccu93c8()
    {
 	   verifyEquals("${literal('-0x1234567890ABCDEF'):toNumber()}", attributes, -0x1234567890ABCDEFL)
    }
 
-   public AIzau5a23u8bf2u59deu9352u9881u5bccu93c9()
+   public AIzau5a23u8bf2u59deu9352u9881u5bccu93c8()
    {
 	   verifyEquals("${literal('-0x1234567890ABCDEF'):toNumber()}", attributes, -0x1234567890ABCDEFL)
    }
